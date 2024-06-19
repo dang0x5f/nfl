ba5812950975e0a9436f20c98d1c7bed1dd87316
dang0x5f <dannypgehrz@gmail.com>
2024-06-19 05:56:29 -0500

    Scroll branch deleted, and merged with main. fe_win removed, fe_win2 -> fe_win. Makefile adjusted for this change.

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
-prog: fe_win2.o file_nodes.o
-	cc -lncurses file_nodes.o fe_win2.o -o prog
+prog: fe_win.o file_nodes.o
+	cc -lncurses file_nodes.o fe_win.o -o prog
 
-fe_win2.o:
-	cc -c fe_win2.c -o fe_win2.o
+fe_win.o:
+	cc -c fe_win.c -o fe_win.o
 
 file_nodes.o:
 	cc -c file_nodes.c -o file_nodes.o
--- fe_win.c
+++ fe_win.c
@@ -1,31 +1,49 @@
 #include <stdlib.h>
 #include <ncurses.h>
 
-void move_cursor(int, WINDOW**);
+#include "file_nodes.h"
+
+void init(void);
+void loop(void);
+
+void move_cursor(int, fnode_t**, int);
+void no_scroll(int, fnode_t**, int);
+void with_scroll(int, fnode_t**, int);
 
 static int maxy, maxx;
 static int y, x;
 
+static int index;
+
 int main(void){
-    int key, quit = FALSE;
+
+    init();
+
+    loop();
+
+    endwin();
+
+    return(EXIT_SUCCESS);
+}
+
+void init(void){
+    index = 0;
 
     initscr();
     start_color();
     init_pair(1, COLOR_BLACK, COLOR_RED);
+    curs_set(0);
+    refresh();
+}
+
+void loop(void){
+    int key, quit = FALSE;
 
     getmaxyx(stdscr, maxy, maxx);
 
-    WINDOW* subwins[maxy];
-
-    for(int i = 0; i < maxy; i++){
-        subwins[i] = derwin(stdscr, 1, maxx, i, 0);
-        /* if( (i % 2) == 0){ */
-        /*     wbkgd(subwins[i], COLOR_PAIR(1) ); */
-        /* } */
-        mvwprintw(subwins[i], 0, 15, "%d", i);
-    }
-        mvwprintw(subwins[0], 0, 25, "%d", maxy);
-    wbkgd(subwins[0], COLOR_PAIR(1));
+    fnode_t* filepads;    
+    int f_cnt = populate_fnodes(&filepads);
+    draw_fnodes(&filepads, f_cnt);
 
     do {
         noecho();
@@ -38,9 +56,7 @@ int main(void){
                 break;
             case 'k':
             case 'j':
-            case 'h':
-            case 'l':
-                move_cursor(key, subwins);
+                move_cursor(key, &filepads, f_cnt);
                 break;
             default:
                 break;
@@ -48,32 +64,79 @@ int main(void){
 
     } while(!quit);
 
-    endwin();
-    return(EXIT_SUCCESS);
+    free_fnodes(&filepads, f_cnt);
 }
 
-void move_cursor(int key, WINDOW* subwins[]){
+void move_cursor(int key, fnode_t** pads, int f_cnt){
+
     getyx(stdscr, y, x);
+
+    if(f_cnt <= maxy - 1)
+        no_scroll(key, pads, f_cnt);
+    else if(f_cnt > maxy - 1)
+        with_scroll(key, pads, f_cnt);
+
+            // debug //
+    /* wbkgd((*pads)[1].fpad, COLOR_PAIR(1)); */
+    /* prefresh((*pads)[1].fpad, 0,0, 1,0,  2,getmaxx(stdscr)); */
+}
+
+void no_scroll(int key, fnode_t** pads, int f_cnt){
+
+    if ( (key == 'k') && (y > 0) ){
+        move(y - 1, x);
+
+        wbkgd( (*pads)[y - 1].fpad, COLOR_PAIR(1));
+        wbkgd( (*pads)[y].fpad,     A_NORMAL);
+
+        prefresh( (*pads)[y - 1].fpad,  0,0,  y-1,0,  y,maxx   );
+        prefresh( (*pads)[y].fpad,      0,0,  y,0,    y+1,maxx );
+    }
+    else if( (key == 'j') && (y < f_cnt - 1) ){
+        move(y + 1, x);
+
+        wbkgd( (*pads)[y + 1].fpad, COLOR_PAIR(1));
+        wbkgd( (*pads)[y].fpad,     A_NORMAL);
+
+        prefresh( (*pads)[y + 1].fpad,  0,0,   y+1,0,  y+2,maxx );
+        prefresh( (*pads)[y].fpad,      0,0,   y,0,    y+1,maxx );
+    }
+}
+
+void with_scroll(int key, fnode_t** pads, int f_cnt){
 
     if( (key == 'k') && (y > 0) ){
         move(y - 1, x);
-        wbkgd(subwins[y-1], COLOR_PAIR(1));
-        wbkgd(subwins[y], A_NORMAL);
-        wrefresh(subwins[y-1]);
-        wrefresh(subwins[y]);
-    }
-    else if( (key == 'j') && (y < maxy - 1) ){
-        move(y + 1, x);
-        wbkgd(subwins[y + 1], COLOR_PAIR(1));
-        wbkgd(subwins[y], A_NORMAL);
-        wrefresh(subwins[y + 1]);
-        wrefresh(subwins[y]);
-    }
-    else if( (key == 'h') && (x > 0) ){
-        move(y, x - 1);
-    }
-    else if( (key == 'l') && (x < maxx) ){
-        move(y, x + 1);
+
+        wbkgd( (*pads)[y + index - 1].fpad, COLOR_PAIR(1) );
+        wbkgd( (*pads)[y + index].fpad,     A_NORMAL      );
+
+        prefresh( (*pads)[y + index - 1].fpad,  0,0,  y-1,0,  y,maxx   );
+        prefresh( (*pads)[y + index].fpad,      0,0,  y,0,    y+1,maxx );
     }
 
+    else if( (key == 'k') && (y == 0) ){
+        ( (--index) < 0 ) ? index = 0 : index;
+        if( (index) >= 0 )
+            refresh_fnodes(pads, index, maxy - 1, 0);
+        move(0,0);
+    }
+
+    else if( (key == 'j') && (y < maxy - 1) ){
+        move(y + 1, x);
+        wbkgd( (*pads)[y + index + 1].fpad, COLOR_PAIR(1) );
+        wbkgd( (*pads)[y + index].fpad,     A_NORMAL      );
+
+        prefresh( (*pads)[y + index + 1].fpad, 0,0, y+1,0,  y+2,maxx );
+        prefresh( (*pads)[y + index].fpad,     0,0, y,0,    y+1,maxx );
+    }
+    else if( (key == 'j') && (y == maxy - 1) ){
+
+        ( (++index + (maxy - 1)) == f_cnt) ? --index : index;
+
+        if( (index + (maxy - 1)) < f_cnt )
+            refresh_fnodes(pads, index, maxy - 1, 1);
+        move(maxy - 1,0);
+    }
 }
+
